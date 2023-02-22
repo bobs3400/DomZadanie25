89619eecdaef27b8828d40362a854466c66c2220
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-02-22 20:51:35 +1000

    25

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int GetStepen(int a, int b)
+{
+    int result = 1;
+    for (int i= 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;  
+}
+Console.WriteLine("Введите число А и В");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"Число А в степени В равно {GetStepen(a, b)}");
